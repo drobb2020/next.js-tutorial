da7414f23d8b3f504d9aa0125cf3580c70aaa1b4
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-07-31 09:21:48 -0400

    End of Video 10

--- pages/404.js
+++ pages/404.js
@@ -28,5 +28,5 @@ const NotFound = () => {
     </>
   );
 }
- 
+
 export default NotFound;
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,17 +1,34 @@
-import Head from 'next/head'
+import Head from "next/head"
+import styles from '../../styles/Ninjas.module.css'
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
-    <Head>
-      <title>Ninja List | All Ninjas</title>
-      <meta name="keywords" content="net ninjas" />
-    </Head>
-    <div>
-      <h1>All Ninjas</h1>
-    </div>
+      <Head>
+        <title>Ninja List | All Ninjas</title>
+        <meta name="keywords" content="net ninjas" />
+      </Head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
     </>
   );
-}
- 
+};
+
 export default Ninjas;
